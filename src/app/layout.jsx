c9deb014848fa40e7fe2fe4d6b390c62bcde0416
annapaleologos <annapaleologos89@gmail.com>
2023-08-23 01:42:39 +0200

    Added darkmode switch in header

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,17 +1,19 @@
 import "./globals.css";
-import Header from '@/components/Header';
-
+import Header from "@/components/Header";
+import Providers from "./Providers";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-      <Header />
-      {/* Navbar */}
-      {/* Searchbox */}
-      
-      
-      {children}</body>
+        <Providers>
+          <Header />
+          {/* Navbar */}
+          {/* Searchbox */}
+
+          {children}
+        </Providers>
+      </body>
     </html>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
-import React from "react";
 import MenuItem from "./MenuItem";
+import DarkModeSwitch from "./DarkModeSwitch";
 import Link from "next/link";
 import { AiFillHome } from "react-icons/ai";
 import { BsFillInfoCircleFill } from "react-icons/bs";
@@ -11,7 +11,8 @@ export default function Header() {
         <MenuItem title="HOME" address="/" Icon={AiFillHome} />
         <MenuItem title="ABOUT" address="/about" Icon={BsFillInfoCircleFill} />
       </div>
-      <div className="">
+      <div className="flex items-center space-x-5">
+        <DarkModeSwitch />
         <Link href="/">
           <h2 className="text-lg">
             <span className="px-2 py-1 mr-1 font-bold rounded-lg bg-amber-500">IMDb</span>
